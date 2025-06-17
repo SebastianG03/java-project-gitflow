b00d37c240a2ee9ae58d5184cebfe85d782cfa94
Dase <104153237+SebastianG03@users.noreply.github.com>
2025-06-17 02:18:11 +0000

    UPDATED:Modificaciones produccion

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
         System.out.println(saludo.GenerarSaludo("David"));
+        System.out.println("Version 1.0.0 lista para produccion");
     }
 }
